ca728e757725e2d1e7781ff0e6c05017e2d894d4
Konstantin <Bunaevkonstantin@bk.ru>
2023-07-02 14:21:10 +0400

    Решение второй задачи по Java

---
+++ src/main/java/Task_002.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task_002 {
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Users\\Константин\\Desktop\\GB\\Java\\JavaHomeWork\\JavaHomeWork\\File.txt");
+        Scanner scanner = new Scanner(file);
+        String line = scanner.nextLine();
+        line = line.replace(":", " = ");
+        line = line.replaceAll("[{\"\"}]", "");
+        String[] result = line.split(", ");
+        String req = "select * from students where ";
+        for (int i = 0; i < result.length; i++) {
+            boolean bool = result[i].contains("null");
+            if (bool == false) {
+                req = req + result[i]+ ", ";
+
+            }
+        }
+        System.out.println(req.replaceAll(", $",""));
+    }
+}
